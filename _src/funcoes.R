8363e11745f0944a9f3016a1b86fc248cd346f40
saramortara <saramortara@gmail.com>
2020-05-17 20:31:34 -0300

    script 00 todo em funcoes

--- _src/funcoes.R
+++ _src/funcoes.R
@@ -1,5 +1,6 @@
 library(zoo)
 library(EpiEstim)
+library(lubridate)
 
 # carraga funcoes em fct, listagem em ordem alfabetica
 source("fct/beta.summary.R")
@@ -12,15 +13,20 @@ source("fct/existe.nowcasting.R")
 source("fct/fitP.exp.R")
 source("fct/forecast.exponential.R")
 source("fct/formata.now.df.R")
+source("fct/gera.nowcasting.R")
 source("fct/get.data.base.R")
 #source("fct/get.data.base2.R")
+source("fct/get.last.date.R")
 source("fct/inv.logit.R")
 source("fct/na.zero.R")
 source("fct/now.proj.R")
 source("fct/preenche.now.R")
 source("fct/prepara.dados.R")
+source("fct/read.sivep.R")
 source("fct/Re.com.data.R")
 #source("fct/tabelas.web.R")
+source("fct/write.notificacoes.data.R")
+source("fct/write.nowcasting.R")
 source("fct/zoo2df.R")
 
 # formatacao e funcoes de plot
