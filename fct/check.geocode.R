946c287ddd1124ef99c69e537ebc7d88d950ff87
saramortara <saramortara@gmail.com>
2020-05-20 12:19:03 -0300

    geocode em check.geocode aceita 6 ou 7 digitos

--- fct/check.geocode.R
+++ fct/check.geocode.R
@@ -1,3 +1,6 @@
+#' Função para extrair nome do path a partir do geocode
+#' @param escala Caractere. Escala do filtro: `"municipio`, `"estado"`, `"micro"`, `"meso"`
+#' @param geocode Caractere. Geocode IBGE. Município 6 ou 7 dígitos; microrregião 5 dígitos; mesorregião 4 dígitos; estado 2 dígitos
 check.geocode <- function(escala,
                           geocode) {
 
@@ -7,8 +10,8 @@ check.geocode <- function(escala,
   # df$nome.nonascii <- gsub("'", "", df$nome.nonascii)
   # write.csv(df, "../dados/geocode_ibge.csv", row.names = FALSE)
   df <- read.csv("../dados/geocode_ibge.csv")
-  geocode <- as.numeric(geocode)
-  municipio.code <- df$id
+  #geocode <- as.numeric(geocode)
+  municipio.code <- sapply(df$id, function(x) substr(x, start = 1, stop = 6))
   micro.code   <- df$microrregiao.id
   meso.code    <- df$microrregiao.mesorregiao.id
   estado.code  <- df$microrregiao.mesorregiao.UF.id
@@ -17,6 +20,8 @@ check.geocode <- function(escala,
     if (escala == "municipio") {
       if (!nchar(geocode) %in% c(6, 7))
         stop("geocode não bate com escala")
+      if (nchar(geocode) == 7)
+        geocode <- substr(geocode, start = 1, stop = 6)
     if (geocode %in% municipio.code) {
       id <- which(municipio.code == geocode)
       nome <- paste0("municipios/", estado.sigla[id], "/", df$nome.nonascii[id]) # municipios/SP/Sao_Paulo
@@ -65,4 +70,3 @@ check.geocode <- function(escala,
 # check.geocode(escala = "micro", geocode = "21")
 # check.geocode(escala = "micro", geocode = "21008")
 # check.geocode(escala = "meso", geocode = "2102")
-
