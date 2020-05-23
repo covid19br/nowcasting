3fee0a12dfc1263e15a5220d7c530bead5c4eebe
saramortara <saramortara@gmail.com>
2020-05-23 11:15:44 -0300

    resolve data e ylim do grafico td (coord_cartesian)

--- fct/plot.tempo.dupl.R
+++ fct/plot.tempo.dupl.R
@@ -1,14 +1,15 @@
 # Funcao para calcular o tempo de duplicacao
 plot.tempo.dupl <- function(df.td) {#data.frame com tempo de duplicacao
     plot <- df.td %>%
-        mutate(data = as.Date(data)) %>% 
+        mutate(data = as.Date(data)) %>%
         ggplot(aes(x = data, y = estimativa)) +
         geom_ribbon(aes(ymin = ic.inf, ymax = ic.sup), fill = "lightgrey") +
         geom_line(size = 1.25, colour = RColorBrewer::brewer.pal(3, "Dark2")[1]) +
         scale_x_date(date_labels = "%d/%b", name = "") +
-        ##coord_cartesian(ylim = c(0, 50)) +
+        # coloca coordenada cartesiana para resolver o problema do lim
+        coord_cartesian(ylim = c(0, 100)) +
+        #ylim(c(0, max(df.td$ic.sup)))
         ylab("Tempo de duplicação (dias)") +
-        ylim(0, max(df.td$ic.sup)) +
-        plot.formatos 
-    plot    
+        plot.formatos
+    plot
 }
