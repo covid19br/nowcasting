e99c47b6f4d46a2bc3e1980eb2cb1687c7294125
Renato Coutinho <renato.coutinho@gmail.com>
2020-06-13 03:31:41 -0300

    função que conta linhas

---
+++ fct/count.lines.R
@@ -0,0 +1,21 @@
+#' Conta número de linhas de um arquivo de texto, zipado ou bzipado ou xzeado
+#' @param filename Caractere. Caminho do arquivo
+count.lines <- function(filename){
+    if (endsWith(filename, 'csv'))
+        return(as.integer(system2('wc',
+                                  args=c('-l', filename, ' | cut -d" " -f1'),
+                                  stdout=T)))
+    else if (endsWith(filename, 'zip'))
+        return(as.integer(system2('zgrep', args=c('-c', '$', filename),
+                                  stdout=T)))
+    else if (endsWith(filename, 'bz2'))
+        return(as.integer(system2('bzgrep', args=c('-c', '$', filename),
+                                  stdout=T)))
+    else if (endsWith(filename, 'xz'))
+        return(as.integer(system2('xzgrep', args=c('-c', '$', filename),
+                                  stdout=T)))
+    else {
+        print("Tipo de arquivo desconhecido")
+        return(0)
+    }
+}
