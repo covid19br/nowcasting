db0d64e7a6a3282be98d916d7445393d1f200221
piklprado <paulo@PI.localdomain>
2020-07-10 15:59:56 -0300

    Nova funcao para calacular atraso mediano a aprtir do nowcasting

--- fct/beta.cumsum.R
+++ fct/beta.cumsum.R
@@ -13,21 +13,21 @@
 #'     portanto podem ser interpretados como a probabilidade cumulativa de casos
 #'     ser notificados D dias após o dias o primeiro sintoma, sendo que
 #'     vai de zero ao máximo definido pelos argumentos do nowcasting
-beta.cumsum <- function(NobBS.output, NobBS.params.post, samples){
+beta.cumsum <- function(NobBS.output, NobBS.params.post, samples){    
     if(missing(NobBS.params.post))
         df <- NobBS.output$params.post
     else
-        df <- nowcasting$params.post
-        df1 <- df[, names(df)[grepl("Beta",names(df))]]
-        if (samples > nrow(df1))
-            stop(paste("samples deve ter tamanho menor ou igual a", nrow(df1)))
-        else
-            df2<-df1[sample(nrow(df1), samples), ]
-            df2<-exp(df2)
-            df3<-t(apply(df2, 1, cumsum))
-            data.frame(atraso = as.integer(substr(colnames(df3), 6, 8)),
+        df <- NobBS.params.post
+    df1 <- df[, names(df)[grepl("Beta",names(df))]]
+    if (samples > nrow(df1))
+        stop(paste("samples deve ter tamanho menor ou igual a", nrow(df1)))
+    else
+        df2<-df1[sample(nrow(df1), samples), ]
+    df2<-exp(df2)
+    df3<-t(apply(df2, 1, cumsum))
+    data.frame(atraso = as.integer(substr(colnames(df3), 6, 8)),
                         mean = apply(df3, 2, mean),
-                        lower = apply(df3, 2, quantile, 0.025),
-                        upper = apply(df3, 2, quantile, 0.975),
-                        row.names = names(df3))
+               lower = apply(df3, 2, quantile, 0.025),
+               upper = apply(df3, 2, quantile, 0.975),
+               row.names = names(df3))
 }
---
+++ fct/median_delay.R
@@ -0,0 +1,44 @@
+#' Tempo mediana de atraso, com intervalos de cerdibilidade
+#' @details Recebe o output do nowcasting feito pela função NobBS ou
+#'     um dataframe com a distribuição acumulada de atrasos (gerada
+#'     pela funão beta.cumsum) e retorna o tempo mediano de atraso,
+#'     com limites de 2.5% e 95% do intervalo de credibilidade.
+#' @param cum.betas dataframe com CDF dos dias de atraso e seus
+#'     intervalos de credibilidade a 95%, retornado pela função
+#'     beta.cumsum. Se um dos dois argumentos seguintes deve estar
+#'     presente.
+#' @param NobBS.output objeto retornado pela função NobBS do pacote de
+#'     mesmo nome Este argumento é ignorado se os argumentos cum.betas
+#'     ou NobBS.params.post é usado.
+#' @param NobBS.params.post data frame com as distribuicoes
+#'     posteriores dos parâmetros estimados pela função NobBS. Está
+#'     contido na lista que é retornada pela função. Este argumento é
+#'     ignorado se os argumento cum.betas é fornecido.
+#' @param samples Tamanho da amostra da distribuição posterior de
+#'     betas. Necessário se argumento cum.betas é omitido.
+#' @return data frame com média e quantis 2.5% e 97.5% do atraso
+#'     mediano estimado pelo nowcasting.  Este atraso mediano é obtido
+#'     por interpolação das distribuição de probabilidade acumualdas
+#'     dos atrasos, estimadas pela função beta.cumsum.
+median_delay <- function(cum.betas, NobBS.params.post, NobBS.output, samples){
+    if(missing(cum.betas)){
+        if(missing(NobBS.params.post))
+            NobBS.params.post <- NobBS.output$params.post
+        cum.betas <- beta.cumsum(NobBS.output=NULL, NobBS.params.post = NobBS.params.post, samples = samples)
+    }
+    result <- sapply(cum.betas[,-1], quantile.cdf, x= cum.betas[,1])
+    names(result) <- names(result)[c(1,3,2)] ## quem é lower e upper limit inverte neste calculo, apenas ajeita rotulos.
+    return(result)
+}
+
+#' Acessory function: estimates quantile(s) by interporlation of a cumulative probability distribution
+#' @param numeric vector of cumulative probabilities
+#' @param numeric vector of quantile at each accumulated probability in cdf
+#' @param numeric vector of acummalated probablities for which the quantile is to be calculated.
+#' @return a vector of quantiles at each value of prob.
+quantile.cdf <- function(cdf, x, prob = 0.5){
+    if(length(cdf)!=length(prob))
+        stop("cdf and prob should be of the same length")
+    f1 <- approxfun(x = cdf, y = x)
+    f1(quant)
+    }
