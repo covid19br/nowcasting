a257b055d2c396177e699e0a8a5dbee0c3fd368c
rafalopespx <lopes1313@gmail.com>
2020-08-27 18:57:06 -0300

    Calculo de betas por estado

---
+++ betas_estados.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# lista de siglas e geocodes de estados
+#declare -A ESTADOS=([12]=AC [27]=AL #[13]=AM [16]=AP [29]=BA [23]=CE [53]=DF #[32]=ES [52]=GO [21]=MA [31]=MG [50]=MS [51]=MT [15]=PA [25]=PB [26]=PE #[22]=PI [41]=PR [33]=RJ [24]=RN [11]=RO [14]=RR [43]=RS [42]=SC [28]=SE #[35]=SP [17]=TO)
+
+declare -A ESTADOS=([35]=SP [33]=RJ)
+
+for VARIABLE in ${!ESTADOS[@]}
+
+do echo 
+	Rscript update_nowcasting.R --escala estado --geocode $VARIABLE --outputDir ./Estados_betas/estado_${ESTADOS[$VARIABLE]} --Rmethod Cori --betas TRUE &
+
+done;
