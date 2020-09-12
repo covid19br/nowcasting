39829dd0e29ed2016b42574636f01fd25a11d02e
Debian <debian@testebio.novalocal>
2020-09-12 15:30:44 -0300

    betas 2020_09_07

--- betas_estados.sh
+++ betas_estados.sh
@@ -2,33 +2,33 @@
 
 # lista de siglas e geocodes de estados
 declare -A ESTADOS=(
-[12]=AC 
-[27]=AL 
-[13]=AM 
-[16]=AP 
-[29]=BA 
-[23]=CE 
-[53]=DF 
+#[12]=AC 
+#[27]=AL 
+#[13]=AM 
+#[16]=AP 
+#[29]=BA 
+#[23]=CE 
+#[53]=DF 
 [32]=ES 
-[52]=GO 
-[21]=MA 
+#[52]=GO 
+#[21]=MA 
 [31]=MG 
-[50]=MS 
-[51]=MT 
-[15]=PA 
-[25]=PB 
-[26]=PE 
-[22]=PI 
-[41]=PR 
-[33]=RJ 
-[24]=RN 
-[11]=RO 
-[14]=RR 
+#[50]=MS 
+#[51]=MT 
+#[15]=PA 
+#[25]=PB 
+#[26]=PE 
+#[22]=PI 
+#[41]=PR 
+#[33]=RJ 
+#[24]=RN 
+#[11]=RO 
+#[14]=RR 
 [43]=RS 
-[42]=SC 
-[28]=SE 
-[35]=SP 
-[17]=TO
+#[42]=SC 
+#[28]=SE 
+#[35]=SP 
+#[17]=TO
 )
 # Está assim só pq é mais fácil comentar estados não rodados
 
